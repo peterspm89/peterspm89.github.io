a779319c92d97b6d538d261387616212af51b8a5
Philip Peters <petersphilm@gmail.com>
2019-04-20 14:25:36 -0400

    Third test created that validates the dimensions of the input grid so that I know I am reading in the text file correctly. Red state.

--- PillarKata/test/WordSearchTest.java
+++ PillarKata/test/WordSearchTest.java
@@ -73,4 +73,40 @@ class WordSearchTest {
         assertArrayEquals(expectedOutput, wordSearch.getOutput());
     }
 
+    @Test
+    public void whenWordSearchIsCreatedTheDimensionsOfGridAreVerified() {
+        /// This third test validates the dimensions of the grid from the input file.
+        /// Per the kata spec, the input files will be a square, so no validation is needed.
+        /// This test assures that I am reading and initializing my grid correctly.
+        /// At this point, I am using the input file that came in the Pillar Technology Kata.
+        /// which is listed below if you expand the region.
+        //region Sample Input file
+        /*
+        BONES,KHAN,KIRK,SCOTTY,SPOCK,SULU,UHURA
+        U,M,K,H,U,L,K,I,N,V,J,O,C,W,E
+        L,L,S,H,K,Z,Z,W,Z,C,G,J,U,Y,G
+        H,S,U,P,J,P,R,J,D,H,S,B,X,T,G
+        B,R,J,S,O,E,Q,E,T,I,K,K,G,L,E
+        A,Y,O,A,G,C,I,R,D,Q,H,R,T,C,D
+        S,C,O,T,T,Y,K,Z,R,E,P,P,X,P,F
+        B,L,Q,S,L,N,E,E,E,V,U,L,F,M,Z
+        O,K,R,I,K,A,M,M,R,M,F,B,A,P,P
+        N,U,I,I,Y,H,Q,M,E,M,Q,R,Y,F,S
+        E,Y,Z,Y,G,K,Q,J,P,C,Q,W,Y,A,K
+        S,J,F,Z,M,Q,I,B,D,B,E,M,K,W,D
+        T,G,L,B,H,C,B,E,C,H,T,O,Y,I,K
+        O,J,Y,E,U,L,N,C,C,L,Y,B,Z,U,H
+        W,Z,M,I,S,U,K,U,R,B,I,D,U,X,S
+        K,Y,L,B,Q,Q,P,M,D,F,C,K,E,A,B
+        */
+        //endregion
+
+        /// Create the expected result.
+        final int expectedDimensions = 15;
+
+        WordSearch wordSearch = new WordSearch();
+        assertEquals(expectedDimensions, wordSearch.getLength());
+        assertEquals(expectedDimensions, wordSearch.getWidth());
+    }
+
 }
